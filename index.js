4e0a54aa7b0309aaf2542404cb8c5b1d2dc2a75b
kd Bromley <kd.bromley1@gmail.com>
2020-11-25 21:40:36 -0800

    updated index.js - add new and delete functional - working on check toggle

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 $(function() {
-    $('.js-shopping-list-form').submit(function(event) {
-        event.preventDefault;
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
         const newItem = $('input#shopping-list-entry').val();
         $('ul').append(`
         <li>
         <span class="shopping-item">${newItem}</span>
         <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
-        <span class="button-label">check</span><
-        /button><button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-        </button>
+        <button class="shopping-item-toggle"><span class="button-label">check</span></button>
+        <button class="shopping-item-delete"> <span class="button-label">delete</span></button>
         </div>
         </li>`);
 
