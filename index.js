9915ab25865dea1c2dd3d87a14106917f9eaef0a
kd Bromley <kd.bromley1@gmail.com>
2020-11-25 21:19:31 -0800

    index.js complete - need check functionality of add and check, but basic code is there

--- index.js
+++ index.js
@@ -14,25 +14,13 @@ $(function() {
         </div>
         </li>`);
 
-    });
-});
-
-/* $(function removeItem() {
+    });    
+    
     $('ul').on('click', '.shopping-item-delete', function(event) {
-        $(this).remove('li')
+        $(this).closest('li').remove();
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+      $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked');
     })
-}) */
-
-// $(function )
-
-/* <li>
-<span class="shopping-item">oranges</span>
-<div class="shopping-item-controls">
-  <button class="shopping-item-toggle">
-    <span class="button-label">check</span>
-  </button>
-  <button class="shopping-item-delete">
-    <span class="button-label">delete</span>
-  </button>
-</div>
-</li> */
+});
