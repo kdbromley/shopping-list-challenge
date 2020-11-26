581ac9c6ba11759f8da11768772b7a393f19d0b8
kd Bromley <kd.bromley1@gmail.com>
2020-11-25 22:06:09 -0800

    added comments to index.js

--- index.js
+++ index.js
@@ -16,12 +16,19 @@ $(function() {
         </div>
         </li>`);
     });    
-    
+    //'ul' in index.html so will exist at page load - use for event listener
+    //specify click on delete button (class="shopping-item-delete")
     $('ul').on('click', '.shopping-item-delete', function(event) {
+        //target closest li - .closest moves upwards so it will target current item li
+        //<li> holds entire current shopping list item including buttons
         $(this).closest('li').remove();
     });
-
+    //ditto 'ul' for event listener
+    //specify click on check button (class="shopping-item-toggle")
     $('ul').on('click', '.shopping-item-toggle', function(event) {
+      // target closest 'li' bc .closest includes current element
+      //so .closest(span) would just target span for check toggle button
+      //<li> high enough in DOM tree to move down w .find() to locate <span> for item string
       $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked');
-    })
+    });
 });
